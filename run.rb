e1527302008683f6a0289589a8fd4dc64b68a109
Charles <unknownsanon@gmail.com>
2023-09-10 15:03:06 +0800

    Added some questions

--- run.rb
+++ run.rb
@@ -1,7 +1,7 @@
 require './quiz_data.rb'
 require './quiz_question.rb'
 
-ruby_quiz = QuizData.new("Ruby Quiz", [
+ruby_quiz = QuizData.new("Ruby Quiz (10 questions)", [
     {
         question: "What type of programming language is Ruby?",
         options: ["Procedural", "Object-oriented", "Functional", "Scripting"],
@@ -16,6 +16,41 @@ ruby_quiz = QuizData.new("Ruby Quiz", [
         question: "Which of the following is the correct way to define a class in Ruby?",
         options: ["class MyClass", "class = MyClass", "Myclass class", "Myclass()"],
         answer: 0,
+    },
+    {
+        question: "Variables that start with two '@' symbols are what kind of variables in Ruby",
+        options: ["Local variable", "Global variable", "Instance variable", "Class variable"],
+        answer: 3,
+    },
+    {
+        question: "Which of the languages syntax matches with the Ruby’s syntax?",
+        options: ["Python", "Perl", "Java", "C++"],
+        answer: 1,
+    },
+    {
+        question: "What is the extension used for saving the ruby file?",
+        options: ["rrb", "ruby", "rb", "rub"],
+        answer: 2,
+    },
+    {
+        question: "Ruby runs on which platform?",
+        options: ["Mac OS", "Linux", "Windows", "All of the Above"],
+        answer: 3,
+    },
+    {
+        question: "What does the .upcase method do?",
+        options: ["Convert the string to lowercase", "Convert the string to uppercase", "Convert only lowercase string to uppercase and vice-versa", "None of above"],
+        answer: 1,
+    },
+    {
+        question: "Ruby was written in which language?",
+        options: ["C++", "C", "Java", "Objective C"],
+        answer: 1,
+    },
+    {
+        question: "What is Ruby?",
+        options: ["markup language", "procedural language", "scripting language", "stylesheet language"],
+        answer: 2,
     },
 ])
 
